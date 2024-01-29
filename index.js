d7f09a71a40494e96a21694b39e68536df92e8ca
MiraculineAshok <miraculinej@gmail.com>
2024-01-29 16:35:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mySql = require("mysql");
+const port = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+
+var con = mySql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "toList"
+});
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+app.get("/api/getTask", function (req, res) { 
+    var sql = "SELECT * FROM tasks";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+    });
+});
+
+app.post("/api/addTask", function (req, res) {
+    var task = req.body.taskName;
+    var status = req.body.taskStatus;
+
+    var sql = "INSERT INTO tasks (taskName,taskStatus) VALUES (?,?)";
+    con.query(sql, [task,status], function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+    });
+})
+
+app.post("/message", (req, res) => {
+    res.json({ message: "Hello from server!" });
+    console.log(req.body.message);
+});
+
+app.post("/api/deleteTask", (req, res) => {
+
+    var id = req.body.taskId;
+    var sql = "DELETE FROM tasks WHERE id ="+id;
+     con.query(sql, function(err,result){
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+     })
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port 9000.`);
+});
